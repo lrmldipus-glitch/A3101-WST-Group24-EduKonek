a1adaede60423c0de286c5e394f452eb7cda311a
JerJerJer0 <lr.jajllanera@mmdc.mcl.edu.ph>
2026-09-21 22:05:28 -0400

    login.js
    sample credentials to just test functionality.

--- login.js
+++ login.js
@@ -1,1 +1,22 @@
+function checkCredentials() {
+    const username = document.getElementById("user-input").value.trim();
+    const password = document.getElementById("pass-input").value.trim();
 
+    // Sample lang po na credentials :> //
+
+    
+    const validUsername = "admin";
+    const validPassword = "password123";
+
+    if (username === "" || password === "") {
+        alert("Please enter both username and password.");
+        return;
+    }
+
+    if (username === validUsername && password === validPassword) {
+        alert("Login successful!");
+        window.location.href = "Dashboard.html"; 
+    } else {
+        alert("Invalid username or password.");
+    }
+}
