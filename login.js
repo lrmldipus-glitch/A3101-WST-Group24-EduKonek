84fe9df5f717012623c3964ba5e5a30cef2f2e7c
JerJerJer0 <lr.jajllanera@mmdc.mcl.edu.ph>
2026-09-21 22:23:37 -0400

    login.js
    Login functionality

--- login.js
+++ login.js
@@ -2,20 +2,17 @@ function checkCredentials() {
     const username = document.getElementById("user-input").value.trim();
     const password = document.getElementById("pass-input").value.trim();
 
-    // Sample lang po na credentials :> //
-
-    
-    const validUsername = "admin";
-    const validPassword = "password123";
-
     if (username === "" || password === "") {
         alert("Please enter both username and password.");
         return;
     }
 
-    if (username === validUsername && password === validPassword) {
-        alert("Login successful!");
-        window.location.href = "Dashboard.html"; 
+    if (username === "Mentor" && password === "12345") {
+        alert("Login successful, Welcome!");
+        window.location.href = "teacher.html";
+    } else if (username === "Student" && password === "12345") {
+        alert("Login successful, Welcome!" );
+        window.location.href = "Student.html";
     } else {
         alert("Invalid username or password.");
     }
